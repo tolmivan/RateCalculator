5b4232ac716d8e2a2216ef24d4948997ad19f6ac
tolmivan <ivan@DESKTOP-MDB3PVK>
2017-02-04 19:07:10 +1100

    Night Rate added

---
+++ RateCalculator.Model/NightRate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateCalculator.Model
+{
+    public class NightRate : RateBase
+    {
+        #region Rate Specific Consts
+        // keeping rate specific constants at the top for readability
+        private readonly TimeSpan _startEntry = new TimeSpan(18, 0, 0);
+        private readonly TimeSpan _endEntry = new TimeSpan(24, 0, 0);
+        private readonly TimeSpan _startExit = new TimeSpan(18, 0, 0);
+        private readonly TimeSpan _endExit = new TimeSpan(30, 0, 0);
+
+        private const decimal DefaultRate = 6.50M;
+
+        #endregion Rate Specific Consts
+
+        public NightRate() : this(DefaultRate) { }
+        
+        public NightRate(decimal dailyRate)
+        {
+            this.RateName = "Night";
+            this.IsSpecial = true;
+            this.RateType = RateTypes.FlatRate;
+            this.RateAmount = dailyRate;
+            this.Notes = "Enter between 6:00 PM to 12:00 AM\nExit before 6 AM the following day";
+        }
+        public override decimal? GetTotal(DateTime entryTime, DateTime exitTime)
+        {
+            if (!VerifyRateApplies(entryTime, exitTime))
+                return null;
+
+
+            return this.RateAmount;
+        }
+
+        public override bool VerifyRateApplies(DateTime entryTime, DateTime exitTime)
+        {
+            TimeSpan actualEntry = entryTime.TimeOfDay;
+            TimeSpan actualExit = exitTime.TimeOfDay;
+
+            if (exitTime.Date > entryTime.Date)
+                actualExit = actualExit.Add(new TimeSpan(1, 0, 0, 0));
+
+            if (exitTime.Date > entryTime.Date.AddDays(1))
+            {
+                return false;
+            }
+
+            // check if entry time falls between the right interval
+            if ((actualEntry >= _startEntry) && (actualEntry <= _endEntry))
+            {
+                // check exit time
+                if ((actualExit >= _startExit) && (actualExit <= _endExit))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ RateCalculator.Test/NightRateTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RateCalculator.Model;
+
+namespace RateCalculator.Test
+{
+    [TestClass]
+    public class NightRateTest
+    {
+        [TestMethod]
+        public void TestValidRangeSameNight()
+        {
+            NightRate rate = new NightRate();
+
+            DateTime entry = Convert.ToDateTime("01/08/2008 19:00:00");
+            DateTime exit = Convert.ToDateTime("01/08/2008 20:00:00");
+
+            Assert.AreEqual(6.50M, rate.GetTotal(entry, exit));
+        }
+
+        [TestMethod]
+        public void TestValidRangeExitNextMorning()
+        {
+            NightRate rate = new NightRate();
+
+            DateTime entry = Convert.ToDateTime("01/08/2008 19:00:00");
+            DateTime exit = Convert.ToDateTime("02/08/2008 05:00:00");
+
+            Assert.AreEqual(6.50M, rate.GetTotal(entry, exit));
+        }
+
+        [TestMethod]
+        public void TestInValidRangeExitNextMorning()
+        {
+            NightRate rate = new NightRate();
+
+            DateTime entry = Convert.ToDateTime("01/08/2008 19:00:00");
+            DateTime exit = Convert.ToDateTime("02/08/2008 06:01:00");
+
+            Assert.AreEqual(null, rate.GetTotal(entry, exit));
+        }
+
+        [TestMethod]
+        public void TestInValidRangeExitAfter2Days()
+        {
+            NightRate rate = new NightRate();
+
+            DateTime entry = Convert.ToDateTime("01/08/2008 19:00:00");
+            DateTime exit = Convert.ToDateTime("03/08/2008 05:00:00");
+
+            Assert.AreEqual(null, rate.GetTotal(entry, exit));
+        }
+
+
+
+        [TestMethod]
+        public void TestInvalidRange()
+        {
+            NightRate rate = new NightRate();
+
+            DateTime entry = Convert.ToDateTime("01/08/2008 07:00:00");
+            DateTime exit = Convert.ToDateTime("01/08/2008 15:00:00");
+
+            Assert.AreEqual(null, rate.GetTotal(entry, exit));
+
+        }
+    }
+}
